c5ab0c863df6c73925088a39dcc51d467b5d3854
lavrenoff <lavr020775@gmail.com>
2022-08-30 16:58:35 +0300

    zadacha25

--- zadacha25/Program.cs
+++ zadacha25/Program.cs
@@ -3,3 +3,32 @@
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
 
+namespace Zadacha25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число А: ");
+            int numA=Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Введите число B: ");
+            int numB=Convert.ToInt32(Console.ReadLine());
+
+            int result=exponentional(numA,numB);
+            Console.Write($"{numA},{numB}={result}");
+        }
+
+        static int exponentional(int A,int B)
+        {
+            int result=1;
+            for(int i=1;i<=B;i++)
+            {
+            result=result*A;
+            }
+            return result;
+        }
+    }
+}
+
+
